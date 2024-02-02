3a21d2520e2d52904262645b42fe518fcc0bbe74
Theinatorinator <Logan.mamanakis@gmail.com>
2024-02-02 15:54:29 -0700

    broken

--- TelemtryManager.py
+++ TelemtryManager.py
@@ -13,29 +13,73 @@
 #    limitations under the License.
 
 import argparse
-from distutils.log import Log
 import logging
+import os
 from git import Repo
 Logger = logging.getLogger(__name__)
 
+_repo = None
 
 def main():
+    # Argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-d', '--debug',
+        '--debug',
         help="Print lots of debugging statements",
-        action="store_const", dest="loglevel", const=logging.DEBUG,
+        action="store_const",
+        dest="logLevel",
+        const=logging.DEBUG,
         default=logging.WARNING,
     )
     parser.add_argument(
-        '-v', '--verbose',
+        '-v',
+        '--verbose',
         help="Be verbose",
-        action="store_const", dest="loglevel", const=logging.INFO,
+        action="store_const",
+        dest="logLevel",
+        const=logging.INFO,
     )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        help="The directory git repo used to store all the logs",
+        action="store",
+        dest="repoPath")
+
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="Force operations to happen, potentially overwriting files.",
+        dest="forceEnabled",
+        action="store_true")
+
     args = parser.parse_args()
     logging.basicConfig(
-        level=args.loglevel, format='%(asctime)s - %(levelname)s: %(message)s')
+        level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
 
+    # Buisnes logic
+    check_and_set_repo(args.repoPath, args.forceEnabled)
+
+
+def check_and_set_repo(repo_path, do_init):
+    if os.path.isdir != True:
+        Logger.critical("Invalid path provided, check your options!")
+        raise RuntimeError("Invalid path")
+
+    _repo = Repo(repo_path)
+
+    if _repo == None:
+        if do_init == False:
+            Logger.critical(
+                """Git repo not found at path! Ensure you provide a path to an
+                    appropriate git repo or use the f flag to force and intialize a new repo at the path.""")
+            raise RuntimeError("No git repo found")
+        else:
+            Logger.info(
+                "No git repo exsists, and we were authorized to create one. Creating!")
+            _repo = Repo.init(repo_path)
+    else:
+        Logger.info("Repo was found at location!")
 
 
 main()
